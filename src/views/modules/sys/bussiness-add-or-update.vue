6d99b5de220f9c5c29bbb56ca506ad9520299ce3
3083 <3083866373@qq.com>
2019-08-07 17:25:47 +0800

    提交

---
+++ src/views/modules/sys/bussiness-add-or-update.vue
@@ -0,0 +1,94 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" label-width="100px">
+      <el-form-item label="业务种类名称" prop="bussinessName">
+        <el-input v-model="dataForm.bussinessName" placeholder="业务种类名称"></el-input>
+      </el-form-item>
+      <el-form-item label="业务种类介绍">
+        <el-input v-model="dataForm.remark" :maxlength="500" type="textarea" placeholder="业务种类介绍"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        visible: false,
+        dataForm: {
+          id: 0,
+          bussinessName: '',
+          remark: ''
+        },
+        dataRule: {
+          bussinessName: [
+            { required: true, message: '业务种类名称不能为空', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      init (id) {
+        this.dataForm.id = id || 0
+        this.visible = true
+        this.$nextTick(() => {
+          this.$refs['dataForm'].resetFields()
+        })
+        if (this.dataForm.id) {
+          this.$http({
+            url: this.$http.adornUrl(`/sys/bussiness/info/${this.dataForm.id}`),
+            method: 'get',
+            params: this.$http.adornParams()
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.dataForm.bussinessName = data.bussiness.bussinessName
+              this.dataForm.remark = data.bussiness.remark
+            }
+          })
+        }
+      },
+      // 表单提交
+      dataFormSubmit () {
+        this.$refs['dataForm'].validate((valid) => {
+          if (valid) {
+            this.$http({
+              url: this.$http.adornUrl(`/sys/bussiness/${!this.dataForm.id ? 'save' : 'update'}`),
+              method: 'post',
+              data: this.$http.adornData({
+                'id': this.dataForm.id || undefined,
+                'bussinessName': this.dataForm.bussinessName,
+                'remark': this.dataForm.remark
+              })
+            }).then(({ data }) => {
+              if (data && data.code === 0) {
+                this.$message({
+                  message: '操作成功',
+                  type: 'success',
+                  duration: 1500,
+                  onClose: () => {
+                    this.visible = false
+                    this.$emit('refreshDataList')
+                  }
+                })
+              } else {
+                this.$message.error(data.msg)
+              }
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
